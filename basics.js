f5728a36070aff43c6fdead3e2a58d4336a7f867
Andrejs Nikolajevs <you@example.com>
2024-12-02 08:59:55 +0200

    commit1

---
+++ basics.js
@@ -0,0 +1,20 @@
+let isStudents = true
+
+if (isStudents = true){
+    console.log("Students!")
+}
+else{
+    console.log("Nav Students!")
+}
+
+let numArray = ["3", "56", "7"]
+
+for (let i = 0; i < numArray.lenght; i++) {
+    console.log("1.skaitlis" + [i])
+}
+
+printErrorStatus()
+
+function printErrorStatus(){
+    
+}
